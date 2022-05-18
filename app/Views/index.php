4ab23013570fbfc73ae096d74a7926c8982e6875
Jhersonnpa <jhersonnpa@gmail.com>
2022-05-19 01:12:28 +0200

    Commit

---
+++ app/Views/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>B4die</title>
+    <link rel="icon" href="<?= base_url('img/icon-b4die.ico')?>">
+    <link rel="stylesheet" href="<?= base_url('css/style.css')?>">
+</head>
+<body>
+    <nav>
+        <div class="nav-top">
+            <img src="<?= base_url('img/icon-search.svg')?>">
+            <img src="<?= base_url('img/icon-user.svg')?>">
+        </div>
+        <div class="nav-bottom">
+            <a href=""><img src="<?= base_url('img/logo.png')?>"></a>
+            <ul>
+                <li><a href="#">Experiencias</a></li>
+                <li><a href="#">Ranking</a></li>
+                <li><a href="#">Mapa</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
